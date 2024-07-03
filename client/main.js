97c973a025b7d277fc233272888f0467eef23782
happyhye <50566@naver.com>
2024-07-03 10:01:06 +0900

    웹 컴포넌트: 탄생, 죽음

--- client/main.js
+++ client/main.js
@@ -1,35 +1,24 @@
-import { 
-  clearContents,
-  deleteStorage,
-  getNode,
-  getStorage,
-  setStorage,
-} from "./lib/index.js";
+class MyElement extends HTMLElement {
 
-const textField = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
+  constructor(){
+    super(); //부모의 능력을 내가 다 상속받겠다.
+  }
 
-getStorage('text')
-.then((res)=>{
-  // console.log(res); //저장된값이 출력됨
+  connectedCallback(){
+    console.log('탄생함');
+  }
 
-  textField.value = res;
-})
-
-function handleTextField(){
-  const value = this.value;
-  console.log(value);
-
-  setStorage('text', value)
-
+  disconnectedCallback(){
+    console.log('죽음');
+  }
 }
 
+// 사용할 태그이름 정의,  클래스 연결
+customElements.define('c-element', MyElement)
 
-
-function handleClear(){
-  deleteStorage('text') //제거
-  clearContents(textField) //input도 초기화
-}
-
-textField.addEventListener('input', handleTextField)
-clearButton.addEventListener('click', handleClear)
+// 태그 생성
+const elem = document.createElement('c-element');
+const app = document.getElementById('app');
+console.log(elem);
+// 추가
+app.appendChild(elem)
