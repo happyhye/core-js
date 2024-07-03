1e33ca95280af20a79ebfdace98e54ece826f5a8
happyhye <50566@naver.com>
2024-07-03 10:37:00 +0900

    웹 컴포넌트: Customized built-in elements

--- client/main.js
+++ client/main.js
@@ -1,45 +1,19 @@
-class Buttn extends HTMLElement {
+class Button extends HTMLButtonElement {
+
   constructor(){
     super();
-
-
-    this.button = document.querySelector('button');
   }
 
   connectedCallback(){
-    this._render();
-  }
-
-  disconnectedCallback() {
 
   }
 
-  static get observedAttributes() { //(3)
-    return ['id'];   //id를 계속 감시해
+  disconnectedCallback(){
+
   }
 
-
-  attributeChangedCallback(name, oldValue, newValue) { // (4)
-    console.log( name, oldValue, newValue );
-
-    if(oldValue !== newValue) {
-      this._render()
-    }
-  }
-
-  _render(){
-    this.button.textContent = this.id
-  }
 
 }
 
-customElements.define('c-button', Buttn)
 
-
-// 외부에 의해 속성을 강제로 바꿈
-const c = document.querySelector('c-button')
-let count = 0;
-
-c.addEventListener('click', ()=>{
-  c.setAttribute('id',  ++count)
-})
+customElements.define('c-button',Button, {extends: 'button'});
