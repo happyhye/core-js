a10a5079ba2b6f1ccb225ff1bdb6f81d2a6725d3
happyhye <50566@naver.com>
2024-07-03 11:15:10 +0900

    웹 컴포넌트: Shaodw DOM

--- client/main.js
+++ client/main.js
@@ -1,7 +1,17 @@
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
 
   constructor(){
     super();
+
+    // c-button의 쉐도우 돔을 열어줘
+    // c-button을 어떻게 잡아? -> this
+    this.attachShadow({mode: 'open'});
+
+    // 쉐도우 돔 안에 원하는 태그 집어넣기
+    this.shadowRoot.innerHTML= /* html */ `
+      <button>hello</button>
+    `
+
   }
 
   connectedCallback(){
@@ -12,8 +22,10 @@ class Button extends HTMLButtonElement {
 
   }
 
-
 }
 
 
-customElements.define('c-button',Button, {extends: 'button'});
+customElements.define('c-button',Button);
+
+console.log( document.querySelector('button') );  //null. 수집 불가능
+console.log( document.querySelector('c-button').shadowRoot.querySelector('button') );  //접근가능
